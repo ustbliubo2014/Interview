46a4b05becb644b53b5fc59b070948cdfffd74d5
ustbliubo <ustb_liubo@qq.com>
2016-06-10 18:47:16 +0800

    InterviewCode : max tree

--- InterviewCode/stack_deque/maxTree.py
+++ InterviewCode/stack_deque/maxTree.py
@@ -15,6 +15,10 @@ __author__ = 'liubo'
     数组必须没有重复元素;根节点最大;时间复杂度O(N),空间复杂度O(N)
 '''
 
+
+import pdb
+
+
 class Node():
     def __init__(self,value=None, left=None, right=None):
         self.value = value
@@ -27,6 +31,7 @@ def popStack_setDict(stack, dict):
     if len(stack) == 0:
         dict[pop_node] = None
     else:
+        print pop_node.value, stack[-1].value
         dict[pop_node] = stack[-1]
 
 
@@ -60,10 +65,12 @@ def get_right_big_dict(array):
 def build_max_tree(array):
     right_big_dict = get_right_big_dict(array)
     left_big_dict = get_left_big_dict(array)
+    pdb.set_trace()
     for index in range(len(array)):
         current_node = array[index]
         left_node = left_big_dict.get(current_node)
         right_node = right_big_dict.get(current_node)
+        print index, left_node.value, right_node.value
         if left_node == None and right_node == None:
             head = current_node
         elif left_node == None:
@@ -85,7 +92,15 @@ def build_max_tree(array):
                 parent_node.left = current_node
             else:
                 parent_node.right = current_node
+    return head
 
 
 if __name__ == '__main__':
-    pass
+    node1 = Node(3)
+    node2 = Node(4)
+    node3 = Node(5)
+    node4 = Node(1)
+    node5 = Node(2)
+    array = [node1, node2, node3, node4, node5]
+    head = build_max_tree(array)
+    # print head.value, head.left.value, head.right.value
